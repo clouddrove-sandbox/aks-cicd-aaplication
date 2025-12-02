07b3b4d1633067f282813218bc9370193b45d06f
Ravi Malvia <159242332+rmalvia-cd@users.noreply.github.com>
2025-12-03 00:25:03 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -7,15 +7,15 @@ app.get("/", (req, res) => {
   res.send(`
     <h2>🎉 Hugin Test App Running Successfully!</h2>
     <p>Secret loaded at runtime:</p>
-    <pre>${process.env.REFRESH_TOKEN_SECRET || "❌ No secret found"}</pre>
-    <pre>${process.env.ACCESS_TOKEN_SECRET || "❌ No secret found"}</pre>
+    <pre>${process.env.REFRESH_TOKEN || "❌ No secret found"}</pre>
+    <pre>${process.env.ACCESS_TOKEN || "❌ No secret found"}</pre>
     <pre>${process.env.DB_URL || "❌ No secret found"}</pre>
   `);
 });
 
 app.listen(PORT, () => {
   console.log(`🚀 App running on port ${PORT}`);
-  console.log(`🔐 REFRESH_TOKEN_SECRET loaded from runtime:`, process.env.REFRESH_TOKEN_SECRET);
-  console.log(`🔐 ACCESS_TOKEN_SECRET loaded from runtime:`, process.env.ACCESS_TOKEN_SECRET);
+  console.log(`🔐 REFRESH_TOKEN loaded from runtime:`, process.env.REFRESH_TOKEN);
+  console.log(`🔐 ACCESS_TOKEN loaded from runtime:`, process.env.ACCESS_TOKEN);
   console.log(`🔐 DB_URL loaded from runtime:`, process.env.DB_URL);
 });
